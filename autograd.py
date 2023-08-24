3ce2ad27d62b665ad843fbf6b911d7dc98221684
Surya Subramanian <73920354+codingwithsurya@users.noreply.github.com>
2023-08-24 18:42:16 -0400

    Delete autograd.py

--- autograd.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-
-weights = torch.ones(4, requires_grad=True)
-for epoch in range(2):
-    model_output = (weights*3).sum()
-    model_output.backward()
-    print(weights.grad)
-    weights.grad.zero_()
