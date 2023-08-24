abc89be1b7cfbf6ff89e1859039cb73b9a949e7e
Surya Subramanian <73920354+codingwithsurya@users.noreply.github.com>
2023-08-24 18:23:00 -0400

    Add files via upload

---
+++ autograd.py
@@ -0,0 +1,9 @@
+import torch
+import numpy as np
+
+weights = torch.ones(4, requires_grad=True)
+for epoch in range(2):
+    model_output = (weights*3).sum()
+    model_output.backward()
+    print(weights.grad)
+    weights.grad.zero_()
